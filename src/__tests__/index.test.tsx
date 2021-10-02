1c69e80ffc8cb52894a96dffe56058466bf7197e
Nishan <nishanbende@gmail.com>
2021-10-02 09:39:01 +0530

    chore: add test cases

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,59 @@
-it.todo('write a test');
+import React from 'react';
+import { useResponsive } from '../useResponsive';
+import { render } from '@testing-library/react-native';
+import { View } from 'react-native';
+
+// In RN Dimension is mocked to 750px
+describe('test responsive styles', () => {
+  it('verifies min width query', () => {
+    const App = () => {
+      const { styles } = useResponsive({
+        initial: {
+          backgroundColor: 'yellow',
+        },
+        query: [
+          {
+            minWidth: 500,
+            style: {
+              backgroundColor: 'black',
+            },
+          },
+        ],
+      });
+
+      return <View testID="test" style={styles} />;
+    };
+
+    const { getByTestId } = render(<App />);
+    const view = getByTestId('test');
+
+    expect(view.props.style).toEqual([
+      { backgroundColor: 'yellow' },
+      { backgroundColor: 'black' },
+    ]);
+  });
+  it('doesnt apply min width query when it shouldnt', () => {
+    const App = () => {
+      const { styles } = useResponsive({
+        initial: {
+          backgroundColor: 'yellow',
+        },
+        query: [
+          {
+            minWidth: 800,
+            style: {
+              backgroundColor: 'black',
+            },
+          },
+        ],
+      });
+
+      return <View testID="test" style={styles} />;
+    };
+
+    const { getByTestId } = render(<App />);
+    const view = getByTestId('test');
+
+    expect(view.props.style).toEqual([{ backgroundColor: 'yellow' }]);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { useResponsive } from './useResponsive';
+export * from './types';
